cc046f5adefa43f5f6a2acafc413e60039da616e
MaxVidom <MaxVidomenko@gmail.com>
2022-08-05 09:55:13 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useSearchParams } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as fetchApi from '../../api/fetch';
 
